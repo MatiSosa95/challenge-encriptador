e65b929a4293eb8a3205dc0b4ed9d603c554e14d
Maty <mati.sosa95@hotmail.com>
2024-02-08 18:26:56 -0300

    v1.2

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 
 function encriptar(){
     let texto= document.querySelector(".caja-texto").value;
-
+    
     if(!texto.match(/^[a-z/s ]+$/)){
         limpiarCajainput();
         return alert("El texto debe contener letras minusculas y sin acentos");
 
     }
+
     
     let textoCifrado= texto.replace(/e/gi,'enter').replace(/i/gi,'imes').replace(/a/gi,'ai').replace(/o/gi,'ober').replace(/u/gi,'ufat');
-    document.querySelector(".texto-resultado").value=textoCifrado;
-    document.querySelector(".caja-texto").value;
+    document.querySelector(".texto-resultado").innerText=textoCifrado;
+    document.querySelector(".contenedor-resultado").style.display= "block";
+    document.querySelector(".mensaje").style.display="none";
     limpiarCajainput();
-
-    document.getElementById("#copia").removeAttribute("hidden");
+    document.getElementById("copia").style.display="block";
 
 }
 
@@ -21,27 +22,29 @@ function encriptar(){
 function desencriptar(){  
     let texto= document.querySelector(".caja-texto").value;
     let textoCifrado= texto.replace(/enter/gi,'e').replace(/imes/gi,'i').replace(/ai/gi,'a').replace(/ober/gi,'o').replace(/ufat/gi,'u');
-    document.querySelector(".texto-resultado").value= textoCifrado;
+    document.querySelector(".texto-resultado").innerText= textoCifrado;
     limpiarCajainput();
-    
+    document.getElementById("copia").style.display="block";
+   
 }
 
 function portapapeles(){
-    let copia= document.querySelector(".texto-resultado").value;
+    let copia= document.querySelector(".texto-resultado").innerText;
     navigator.clipboard.writeText(copia)
     .then(() => {console.log("Texto Copiado en portapapeles")})
     .catch(err => {console.log("Algo salio mal",err)});
     limpiarCajaOutput();
     alert("Texto Copiado");
     
+    
 }
 
 function limpiarCajainput(){
-    let limpiar= document.querySelector(".caja-texto").value="";
+    document.querySelector(".caja-texto").value="";
 }
 
 function limpiarCajaOutput(){
-    let limpiar= document.querySelector(".texto-resultado").value="";
+    document.querySelector(".texto-resultado").innerText="";
 }
 
 
